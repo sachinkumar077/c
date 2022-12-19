daaaa0cf830ec866ea9dd0eac7cbf3775f342504
imthesk01 <120002295+sachinkumar077@users.noreply.github.com>
2022-12-19 13:24:52 +0530

    function.c

---
+++ test 3.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>  
+int addmult (int,int);
+int main()
+{
+    int i=3,j=4,k,l;
+    k = addmult(i,j);
+    i = addmult(i,j);
+    printf("%d%d\n",k,l);
+    return 0;
+}
+int addmult(int ii,int jj)
+{
+    int kk,ll;
+    kk = ii+jj;
+    ll = ii*jj;
+    return (kk,ll);
+}
+
+    
+
+
